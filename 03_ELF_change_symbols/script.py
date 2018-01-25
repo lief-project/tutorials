daac9e495a4cae7e51d141b1c6231af82613c211
Romain Thomas <romainthomasc@gmail.com>
2018-01-25 13:31:05 +0100

    Fix Python2.7 error
    Resolve #1

--- 03_ELF_change_symbols/script.py
+++ 03_ELF_change_symbols/script.py
@@ -5,14 +5,14 @@ libm   = lief.parse("/usr/lib/libm.so.6")
 
 
 def swap(obj, a, b):
-    symbol_a = next(filter(lambda e : e.name == a, obj.dynamic_symbols))
-    symbol_b = next(filter(lambda e : e.name == b, obj.dynamic_symbols))
+    symbol_a = [e for e in obj.dynamic_symbols if e.name == a].pop()
+    symbol_b = [e for e in obj.dynamic_symbols if e.name == b].pop()
     b_name = symbol_b.name
     symbol_b.name = symbol_a.name
     symbol_a.name = b_name
 
-hashme_pow_sym = next(filter(lambda e : e.name == "pow", hashme.imported_symbols))
-hashme_log_sym = next(filter(lambda e : e.name == "log", hashme.imported_symbols))
+hashme_pow_sym = [e for e in hashme.imported_symbols if e.name == "pow"].pop()
+hashme_log_sym = [e for e in hashme.imported_symbols if e.name == "log"].pop()
 
 hashme_pow_sym.name = "cos"
 hashme_log_sym.name = "sin"
