ecbe997fed4536273368790d153816ca6eabc859
Romain Thomas <romainthomasc@gmail.com>
2018-07-12 14:21:02 +0200

    Remove bind now if present. Fix #2

--- 05_ELF_infect_plt-got/hook_pltgot.py
+++ 05_ELF_infect_plt-got/hook_pltgot.py
@@ -14,4 +14,19 @@ my_memcmp      = hook.get_symbol("my_memcmp")
 my_memcmp_addr = segment_added.virtual_address + my_memcmp.value
 
 crackme.patch_pltgot('memcmp', my_memcmp_addr)
+
+# Remove bind now if present
+if lief.ELF.DYNAMIC_TAGS.FLAGS in crackme:
+    flags = crackme[lief.ELF.DYNAMIC_TAGS.FLAGS]
+    flags.remove(lief.ELF.DYNAMIC_FLAGS.BIND_NOW)
+
+if lief.ELF.DYNAMIC_TAGS.FLAGS_1 in crackme:
+    flags = crackme[lief.ELF.DYNAMIC_TAGS.FLAGS_1]
+    flags.remove(lief.ELF.DYNAMIC_FLAGS_1.NOW)
+
+# Remove RELRO
+if lief.ELF.SEGMENT_TYPES.GNU_RELRO in crackme:
+    crackme[lief.ELF.SEGMENT_TYPES.GNU_RELRO].type = lief.ELF.SEGMENT_TYPES.NULL
+
+
 crackme.write("crackme.hooked")
