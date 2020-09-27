3a35dc68e3a9a3b6f3adec59f040358be761716e
Adrien Guinet <adrien@guinet.me>
2020-09-27 11:56:16 +0200

    Fix the bin2lib tutorial
    Linked to https://github.com/lief-project/LIEF/pull/469

--- 08_ELF_bin2lib/bin2lib.py
+++ 08_ELF_bin2lib/bin2lib.py
@@ -9,5 +9,6 @@ path    = sys.argv[1]
 address = int(sys.argv[2], 16)
 
 app = lief.parse(path)
-app.add_exported_function(address, "check")
+app[lief.ELF.DYNAMIC_TAGS.FLAGS_1].remove(lief.ELF.DYNAMIC_FLAGS_1.PIE)
+app.add_exported_function(address, "check_found")
 app.write("libcrackme101.so")
